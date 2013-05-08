b5ff1057fb0ce1413a1bd7a4eb1d528780a13b19
Greg Billetdeaux <Gregadeaux@gmail.com>
2013-05-08 14:40:56 -0500

    Error 2

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'slim'
 require 'sequel'
 
-DB = Sequel.postgres('d94qvgiuv0n8vi', :user=>'gnswyxprjitnfe', :password=>'7qinf5GbGQgAZq7DWFgN-ok-9h', :host=>'ec2-54-235-155-182.compute-1.amazonaws.com', :port=>5432, max_connections=>20)
+DB = Sequel.postgres('d94qvgiuv0n8vi', :user=>'gnswyxprjitnfe', :password=>'7qinf5GbGQgAZq7DWFgN-ok-9h', :host=>'ec2-54-235-155-182.compute-1.amazonaws.com', :port=>5432, :max_connections=>20)
 
 get '/' do
   slim :index
