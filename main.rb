445fde9d4e04fcd8bdb9822a55d7af7292f5bbda
Greg Billetdeaux <Gregadeaux@gmail.com>
2013-05-08 15:41:39 -0500

    database working

--- main.rb
+++ main.rb
@@ -1,16 +1,22 @@
 require 'sinatra'
 require 'slim'
 require 'sequel'
+require 'pg'
 
-DB = Sequel.postgres('d94qvgiuv0n8vi', :user=>'gnswyxprjitnfe', :password=>'7qinf5GbGQgAZq7DWFgN-ok-9h', :host=>'ec2-54-235-155-182.compute-1.amazonaws.com', :port=>5432, :max_connections=>20)
+before do
+  DB = Sequel.postgres('d94qvgiuv0n8vi', :user=>'gnswyxprjitnfe', :password=>'7qinf5GbGQgAZq7DWFgN-ok-9h', :host=>'ec2-54-235-155-182.compute-1.amazonaws.com', :port=>5432, :max_connections=>20)
+  @items = DB[:items];
+end
 
 get '/' do
+  @all = DB["Select * from items"]
   slim :index
 end
 
 post '/' do
   @task = params[:task]
-  slim :task
+  @items.insert(:name => @task)
+  redirect to('/')
 end
 
 get '/:task' do
